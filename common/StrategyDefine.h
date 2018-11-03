6be39c4fbef691815d67eaf06c707726e64ecff6
huyifeng <huyifeng@baidu.com>
2018-11-03 23:20:51 +0800

    fix StrategyDefine.h

--- common/StrategyDefine.h
+++ common/StrategyDefine.h
@@ -1,4 +1,5 @@
-#pragma once
+#ifndef _STRATEGY_DEFINE_H_
+#define _STRATEGY_DEFINE_H_
 #include "StrategyData.h"
 #include "StrategyContext.h"
 #include "StrategyInquiryDataInterface.h"
@@ -15,7 +16,6 @@
 #include "boost/serialization/split_member.hpp"  
 #include "boost/serialization/utility.hpp"
 #include "boost/serialization/list.hpp"
-
 #include <istream>
 #include <ostream>
 using namespace boost::posix_time;
@@ -44,20 +44,25 @@ class STRATEGY_INTERFACE MStrategy
 {
 public:
 
-	virtual TProbeStructType GetProbeStruct() = 0;								//获取策略绘图探针结构体首地址
+        //Get address of strategy graph prob struct
+	virtual TProbeStructType GetProbeStruct() = 0;
 
-	virtual CParNode * GetParamStruct() = 0;									//获取策略参数结构体首地址
+        //Get the strategy which store the param
+	virtual CParNode * GetParamStruct() = 0;
 
-	virtual bool OnInquiry(MStrategyInquiryDataInterface*) = 0;					//策略自定义交互
+        //Response when received a customed inquery
+	virtual bool OnInquiry(MStrategyInquiryDataInterface*) = 0;
 
-	virtual bool IsSupport(TStrategyTickType) = 0;								//策略是否支持该种类型的tick
+	virtual bool IsSupport(TStrategyTickType) = 0;
 
-	virtual TLastErrorIdType OnInit(ptime CurrentTime) = 0;						//策略初始化
+	virtual TLastErrorIdType OnInit(ptime CurrentTime) = 0;
 
-	virtual TLastErrorIdType OnInit_FromArchive(ptime CurrentTime) = 0;			//从存档文件载入后的OnInit
+	virtual TLastErrorIdType OnInit_FromArchive(ptime CurrentTime) = 0;
 
-	virtual void OnTick(TMarketDataIdType, const CTick *) = 0;					//行情推送
+        //Market data callback
+	virtual void OnTick(TMarketDataIdType, const CTick *) = 0;
 
+        //Order has traded callback
 	virtual void OnTrade(
 		TOrderRefIdType,
 		TOrderSysIdType,
@@ -65,8 +70,9 @@ public:
 		TPriceType,
 		TOrderDirectionType,
 		TOrderOffsetType
-		) = 0;																	//报单有成交
+		) = 0;	
 
+        //Order status has changed callback
 	virtual void OnOrder(
 		TOrderRefIdType,
 		TOrderSysIdType,
@@ -75,28 +81,28 @@ public:
 		TPriceType,
 		TTradedVolumeType,
 		TRemainVolumeType
-		) = 0;																	//报单状态发生变化
+		) = 0;
 
-	virtual void OnEndup() = 0;													//卸载策略时调用
+	virtual void OnEndup() = 0;
 
-	virtual void OnRelease() = 0;												//释放策略
+	virtual void OnRelease() = 0;
 
-	virtual void OnSave(const char * ) = 0;										//保存策略
+	virtual void OnSave(const char * ) = 0;
 
-	virtual void OnLoad(const char * ) = 0;										//加载策略
+	virtual void OnLoad(const char * ) = 0;
 	
-	virtual bool OnGetPositionInfo(int *) = 0;									//获取策略当前持仓，如果策略不支持此功能，返回false即可
+	virtual bool OnGetPositionInfo(int *) = 0;
 
-	virtual bool OnGetCustomInfo(char *, size_t) = 0;							//获取用户自定义信息，如果策略不支持此功能，返回false即可
+	virtual bool OnGetCustomInfo(char *, size_t) = 0;
 
-	virtual bool OnGetFloatingProfit(double *) = 0;								//获取当前持仓浮动盈亏，如果策略不支持此功能，返回false即可
+	virtual bool OnGetFloatingProfit(double *) = 0;	
 
-	virtual bool OnGetStatus(char *, size_t) = 0;								//获取策略状态，如果策略不支持此功能，返回false即可
+	virtual bool OnGetStatus(char *, size_t) = 0;
 };
 
 
 
-
+#endif
 
 
 
