3f5d7ece6cd6215d6821aa304cbe22c541aea913
huyifeng <huyifeng@baidu.com>
2019-01-15 12:30:33 +0800

    update monitor

--- monitor/TradeServerPanel.cpp
+++ monitor/TradeServerPanel.cpp
@@ -354,7 +354,8 @@ afx_msg LRESULT CTradeServerPanel::OnMsgTabChanged(WPARAM wParam, LPARAM lParam)
     }
     catch (std::exception & err)
     {
-        ShowMessage("OnMsgTabChanged:%s", err.what());
+        ShowMessage("Communicate with remote thunder-trader fails. Please check your network[%s:%u] and try again.[%s]", 
+            m_strIp.c_str(), m_uPort, err.what());
         return 0;
     }
     auto TypeNode = result.find("type");
